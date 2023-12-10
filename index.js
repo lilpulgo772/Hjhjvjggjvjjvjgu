b36642c9a51c2e582890b7796a2420ad6432c89a
lilpulgo772 <151698519+lilpulgo772@users.noreply.github.com>
2023-12-10 17:53:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -17,35 +17,38 @@ const workoutSchema = new Schema({
     duration_notes: String,
     exercise_notes: String,
     exercises: Array,
-    id: String
+    id: String,
 });
 
 const Workout = mongoose.model('Workout', workoutSchema);
 
 function proxyGenerator() {
-    const url = 'https://sslproxies.org/';
+    const proxyUrl = 'https://sslproxies.org/';
     let ipAddresses = [];
     let portNumbers = [];
     let randomNumbers = Math.floor(Math.random() * 100);
 
     return new Promise((resolve, reject) => {
-        request(url, function (error, response, html) {
-            if (!error && response.statusCode == 200) {
-                const $ = cheerio.load(html);
-                $("td:nth-child(1)").each(function (index, value) {
-                    ipAddresses[index] = $(this).text();
-                });
-                $("td:nth-child(2)").each(function (index, value) {
-                    portNumbers[index] = $(this).text();
-                });
-            } else if (error) {
-                reject(error);
+        request(proxyUrl, function (error, response, html) {
+            if (error || response.statusCode !== 200) {
+                reject(error || `Failed to fetch proxies, status code: ${response.statusCode}`);
             }
 
-            ipAddresses.join(',');
-            portNumbers.join(',');
+            const $ = cheerio.load(html);
+            $("td:nth-child(1)").each(function (index, value) {
+                ipAddresses[index] = $(this).text();
+            });
+
+            $("td:nth-child(2)").each(function (index, value) {
+                portNumbers[index] = $(this).text();
+            });
+
+            if (!ipAddresses.length || !portNumbers.length) {
+                reject('No proxies found');
+            }
+
             ipAddresses.push('0.0.0.0');
-            let proxy = `http://${ipAddresses[randomNumbers]}:${portNumbers[randomNumbers]}`;
+            const proxy = `http://${ipAddresses[randomNumbers]}:${portNumbers[randomNumbers]}`;
             resolve(proxy);
         });
     });
@@ -64,7 +67,6 @@ function addWorkoutToDatabase(data) {
             }
 
             await Workout.create({ name, author, types, name_notes, duration, duration_notes, exercise_notes, exercises, id });
-
         } catch ({ message }) {
             console.log(message);
         }
@@ -72,37 +74,33 @@ function addWorkoutToDatabase(data) {
 }
 
 function requestHandler(query, newProxy) {
-    let proxy = newProxy;
-    let endpoint = `http://woddrive-legacy-service.cfapps.io/getWod?type=${query}`;
-    let options = url.parse(endpoint);
-    let agent = new httpProxyAgent(proxy);
+    const endpoint = `http://woddrive-legacy-service.cfapps.io/getWod?type=${query}`;
+    const options = url.parse(endpoint);
+    const agent = new httpProxyAgent(newProxy);
     options.agent = agent;
-    let chunks = [];
+    const chunks = [];
 
     return new Promise((resolve, reject) => {
-        try {
-            http.get(options, function (res) {
-                res.on('data', chunk => chunks.push(chunk));
-                res.on('error', reject);
-                res.on('end', () => {
-                    const responseString = Buffer.concat(chunks).toString('utf8');
-                    try {
-                        const parsedResponse = JSON.parse(responseString);
-                        resolve(parsedResponse);
-                    } catch (error) {
-                        reject(error);
-                    }
-                });
+        const req = http.get(options, function (res) {
+            res.on('data', chunk => chunks.push(chunk));
+            res.on('error', reject);
+            res.on('end', () => {
+                const responseString = Buffer.concat(chunks).toString('utf8');
+                try {
+                    const parsedResponse = JSON.parse(responseString);
+                    resolve(parsedResponse);
+                } catch (error) {
+                    reject(error);
+                }
             });
-        } catch ({ message }) {
-            console.log(message);
-        }
+        });
+
+        req.on('error', reject);
     });
 }
 
 (async function () {
     const PORT = 3000;
-    const HOST = '0.0.0.0';
     const DB_URL = 'mongodb://mongo:1Bd1hhgc-h3Hd5d1BFHAfH32h1AFc3gE@monorail.proxy.rlwy.net:55325';
     const myQuery = 'hero';
     const numberOfRequests = 200;
@@ -110,14 +108,14 @@ function requestHandler(query, newProxy) {
     let generateNewProxy = true;
 
     try {
-        const isConnected = await mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
-        if (isConnected) console.log(`Server up and running on port ${PORT}`);
+        await mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log(`Server up and running on port ${PORT}`);
 
         do {
             newProxy = await proxyGenerator();
             generateNewProxy = false;
-            console.log(`using proxy server ${newProxy}`);
-        } while (generateNewProxy === true);
+            console.log(`Using proxy server ${newProxy}`);
+        } while (generateNewProxy);
 
         for (let i = 0; i < numberOfRequests; i++) {
             try {
@@ -129,12 +127,9 @@ function requestHandler(query, newProxy) {
             }
         }
     } catch ({ message }) {
-        console.log(message);
+        console.error(message);
+    } finally {
+        mongoose.disconnect();
+        process.exit(0);
     }
 })();
-
-process.on('SIGINT', () => {
-    console.log(`shutting down, disconnecting from db...`);
-    mongoose.disconnect();
-    process.exit(0);
-});
